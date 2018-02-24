4f5c8afb2b8028a197ae913f2c36b6096cb4e30a
frankhart2017 <sdharchou@gmail.com>
2018-02-24 17:36:48 +0530

    Code completed dataset incomplete

--- EXPLAIN/main.py
+++ EXPLAIN/main.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import pygame
 import os
+from gtts import gTTS
+import random
 
 # Importing the dataset
 dataset = pd.read_csv('details.csv', header=None)
@@ -20,3 +22,38 @@ for x in headings:
             abbre += y[0]
     abbr_headings.append(abbre)
 
+# Function to convert text to audio and play mp3 file
+def audio(message):
+    myobj = gTTS(text=message, lang='en', slow=False)
+    file_path = "welcome" + str(random.randint(1,101)) + ".mp3"
+    myobj.save(file_path)
+    pygame.mixer.init(26000)
+    pygame.mixer.music.load(file_path)
+    pygame.mixer.music.play()
+
+# Take user input of string
+list_direc = os.listdir()
+for x in list_direc:
+    if "mp3" in x:
+        os.remove(x)
+while True:
+    details = input("What do you want to know? ")
+    if details == "1":
+        break
+    details = details.upper()
+    sorry_message = "Sorry, I do not recognize what you said."
+    i = 0
+    while i<len(headings):
+        if details == headings[i]:
+            audio(explanations[i])
+            flag = 0
+            break
+        elif details == abbr_headings[i]:
+            audio(explanations[i])
+            flag = 0
+            break
+        else:
+            flag = 1
+        i += 1
+    if flag == 1:
+        audio(sorry_message) 
